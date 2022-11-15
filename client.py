eb5c3ee6582d4c9b0e5539ca0e10b810e3b4bfd2
NicWell <wellingtonngc12345@gmail.com>
2022-11-15 15:09:24 -0300

    codigo relacionado as conexoes cliente e servidor

---
+++ client.py
@@ -0,0 +1,11 @@
+from socket import *
+
+serverName = '127.0.0.1'
+serverPort = 10100
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+print("Digite um intervalo separado por uma barra\n Exemplo: 1 / 100\n")
+number1 = str(input(":"))
+clientSocket.sendto(number1.encode(),(serverName, serverPort))
+env, serverAddress = clientSocket.recvfrom(2048)
+print("O número Sorteado é: ", env.decode())
+clientSocket.close()
---
+++ server.py
@@ -0,0 +1,22 @@
+from socket import *
+import random
+serverPort = 10100
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('',serverPort))
+print("Servidor Disponivel")
+listInt = [2]
+a = 0 
+b = 0
+while True:
+    numberInterval, clientAddress = serverSocket.recvfrom(2048)
+    numberFix= numberInterval
+    decod = numberFix.decode()
+    listInterval = decod.split("/")
+    a = int(listInterval[0])
+    b = int(listInterval[1])
+    sort = random.randint(a,b)
+    env = str(sort)
+    serverSocket.sendto(env.encode(), clientAddress)
+    if(serverSocket.send):
+        break
+    
